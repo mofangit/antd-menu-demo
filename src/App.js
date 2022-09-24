0f54ae596028b82fec8cb64ed711b75f4de5e60f
dingzengtao <1214267485@qq.com>
2022-09-24 10:46:52 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Meuns from './Menus'
+
+const App = () => {
+  return (
+    <div>
+      <div className="left" style={{ height: '1000px' }}>
+        <Meuns></Meuns>
+      </div>
+    </div>
+  )
+}
+
+export default App
