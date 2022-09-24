d6400afb52f3ac7baab13317dac9329b0d0670eb
dingzengtao <1214267485@qq.com>
2022-09-24 11:06:49 +0800

    init

--- src/Menus/index.js
+++ src/Menus/index.js
@@ -1,18 +1,53 @@
 import {
+  AppstoreOutlined,
+  ContainerOutlined,
+  DesktopOutlined,
+  MailOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
+  PieChartOutlined,
 } from '@ant-design/icons'
 import { Layout, Menu } from 'antd'
 import React, { useState } from 'react'
 import './index.css'
 const { Header, Sider, Content } = Layout
 
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  }
+}
+
+const items = [
+  getItem('Option 1', 'Option 1', <PieChartOutlined />),
+  getItem('Option 2', 'Option 2', <DesktopOutlined />),
+  getItem('Option 3', 'Option 3', <ContainerOutlined />),
+  getItem('Navigation One', 'sub1', <MailOutlined />, [
+    getItem('Option 5', 'Option 4'),
+    getItem('Option 6', 'Option 5'),
+    getItem('Option 7', 'Option 6'),
+    getItem('Option 8', 'Option 7'),
+  ]),
+  getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
+    getItem('Option 9', 'Option 8'),
+    getItem('Option 10', 'Option 9'),
+    getItem('Submenu', 'sub3', null, [
+      getItem('Option 11', 'Option 10'),
+      getItem('Option 12', 'Option 11'),
+    ]),
+  ]),
+]
+
 const Menus = () => {
   const [collapsed, setCollapsed] = useState(false)
-
+  const [key, setKey] = useState('Option 1')
+  const onClick = ({ item, key, keyPath, domEvent }) => {
+    setKey(key)
+  }
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
@@ -20,24 +55,9 @@ const Menus = () => {
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
+          defaultSelectedKeys={['Option 1']}
+          items={items}
+          onClick={onClick}
         />
       </Sider>
       <Layout className="site-layout">
@@ -58,7 +78,7 @@ const Menus = () => {
             minHeight: 280,
           }}
         >
-          Content
+          {key}
         </Content>
       </Layout>
     </Layout>
